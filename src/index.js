808122bbf166565f8ffa3cf99614b6089dacddde
ljc310@126.com <ljc310@126.com>
2019-12-20 18:20:29 +0800

    useModal

--- src/index.js
+++ src/index.js
@@ -3,28 +3,32 @@ import React, { useReducer, createContext, useContext } from 'react'
 const storesMap = {}
 const contextsMap = {}
 
-const createSetFn = (store, dispatch) => newState => {
-  if (newState === null || typeof newState !== 'object') {
-    console.error(`setState传值不正确，请传入对象格式`)
-    return
-  }
-  store._cacheState_ = store._cacheState_ || { ...store.state }
-  Object.assign(store._cacheState_, newState)
-  return Promise.resolve().then(() => {
-    if (store._cacheState_) {
-      dispatch({ type: 'default', state: { ...store._cacheState_ } })
-      store._cacheState_ = null
+const useModel = (initState) => {
+  const reducer = (state, action = {}) => action.state
+  const [state, dispatch] = useReducer(reducer, initState)
+  let cacheState = null
+  const setState = newState => {
+    if (newState === null || typeof newState !== 'object') {
+      console.error(`setState传值不正确，请传入对象格式`)
+      return
     }
-    return store.state
-  })
+    cacheState = cacheState || { ...state }
+    Object.assign(cacheState, newState)
+    return Promise.resolve().then(() => {
+      if (cacheState) {
+        dispatch({ type: 'default', state: cacheState })
+        cacheState = null
+      }
+      return state
+    })
+  }
+  return [state, setState]
 }
 
-const reducer = (state, action = {}) => action.state
-
-const ContextProvider = ({ children, context, store }) => {
-  const [state, dispatch] = useReducer(reducer, store.initialState)
-  store.state = state // 将state绑定到store上，用于渲染和读取
-  store.setState = createSetFn(store, dispatch) // 用于更新状态（异步）
+const StoreProvider = ({ children, context, store }) => {
+  const [state, setState] = useModel(store.initialState)
+  store.state = state // 将state绑定到store上
+  store.setState = setState // 将setState绑定到store上
   return <context.Provider value={state}>
     {children}
   </context.Provider>
@@ -47,9 +51,9 @@ export const Provider = ({ children, ...stores }) => {
     storesMap[key] = store
     contextsMap[key] = context
 
-    return <ContextProvider context={context} store={store}>
+    return <StoreProvider context={context} store={store}>
       {nextKeys.length > 0 ? renderProvider(nextKeys) : children}
-    </ContextProvider>
+    </StoreProvider>
   }
 
   return renderProvider(keys)
